4f31eb2e50b71c029cf01992f981678e2b878a5b
Chiel Zwinkels <chiel.zwinkels@gmail.com>
2026-04-16 23:49:18 +0200

    Initial Commit

---
+++ TernaryCalc/TernaryCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  TernaryCalcApp.swift
+//  TernaryCalc
+//
+//  Created by Chiel Zwinkels on 16/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct TernaryCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
